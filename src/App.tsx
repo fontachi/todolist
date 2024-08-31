9f40b4d5df94c52d5c4eaca6a0884a8671028671
fontachi <Savilovich.Ihar@gmail.com>
2024-08-31 19:08:22 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {TasksType} from  "./TodoList"
+
+import {TodoList} from "./TodoList";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let tasks1:Array<TasksType> = [
+        {id:1,title:"SCC",isDone: true},
+        {id:2,title:"JS",isDone: true},
+        {id:3,title:"React",isDone: false},
+    ]
+
+    return (
+        <div className="App">
+            <TodoList title="What to learn" tasks = {tasks1} />
+        </div>
+    );
 }
 
 export default App;
